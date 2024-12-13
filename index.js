9a0dcd4aeed40a6ad9e1a2aea802eef643d89a1e
Michael Sageryd <michael@sageryd.se>
2024-12-13 23:13:03 +0100

    Got rid of all picker fields in order to get rid of ViewPropTypes

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 'use strict';
 
-import {Form} from './src/Form';
-import {Separator} from './src/fields/Separator';
-import {InputField} from './src/fields/InputField';
-import {LinkField} from './src/fields/LinkField';
-import {SwitchField} from './src/fields/SwitchField';
-import {PickerField} from './src/fields/PickerField';
-import {DatePickerField} from './src/fields/DatePickerField';
-import {TimePickerField} from './src/fields/TimePickerField';
-
+import { Form } from './src/Form';
+import { Separator } from './src/fields/Separator';
+import { InputField } from './src/fields/InputField';
+import { LinkField } from './src/fields/LinkField';
+import { SwitchField } from './src/fields/SwitchField';
 
 //import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
-export {
-        Form,
-        Separator, InputField, LinkField,
-        SwitchField, PickerField, DatePickerField,
-        TimePickerField
-      }
+export { Form, Separator, InputField, LinkField, SwitchField };
